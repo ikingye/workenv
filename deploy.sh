2afe7d05e105963ecfb14e7776127beaed4c03d4
yewang <yewang@baidu.com>
2020-12-14 01:36:13 +0800

    rebuilding site Mon Dec 14 01:36:12 CST 2020

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ msg="rebuilding site $(date)"
 # update the theme
 git submodule update --remote
 # Build the project.
-hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+hugo
 
 # push to source repo
 git add .
